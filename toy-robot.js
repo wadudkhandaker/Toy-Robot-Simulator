a43933729becc62e1a160c9009ea76fb2baf2ac6
SELISE\wadud.khandaker <wadud.khandaker@selise.ch>
2018-09-26 16:05:20 +0600

    add npm link for sync command line and source and add recursive function for receive command

--- toy-robot.js
+++ toy-robot.js
@@ -1,2 +1,13 @@
 #!/usr/bin/env node
 const readline = require('readline');
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+const recursiveAsyncReadLine = function () {
+    rl.question('Enter Command: ', function (answer) {
+        console.log(answer);
+    });
+};
+
+recursiveAsyncReadLine();
